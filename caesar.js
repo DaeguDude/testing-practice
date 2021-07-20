e71b68519e72c43bc711d562c05abffe005fc71a
Sanghak Kim <k3hppk@paycrux.co.kr>
2021-07-20 20:58:52 +0900

    Update caesar to pass num shift positive case

---
+++ caesar.js
@@ -0,0 +1,76 @@
+function caesar(str, numShift) {
+  // Only shift... alphabet
+  let result = "";
+
+  // How do I iterate string?
+  for (let i = 0; i < str.length; i++) {
+    if (isCharacterALetter(str[i])) {
+      // transforming letters into numbers
+      const letterAsNumber = str[i].charCodeAt();
+      let shiftedChar = "";
+
+      if (isLowerCase(str[i])) {
+        const zeroBasedLetter = letterAsNumber - 97;
+
+        const result = ((zeroBasedLetter + numShift) % 26) + 97;
+        shiftedChar = String.fromCharCode(result);
+      } else {
+        const zeroBasedLetter = letterAsNumber - 65;
+
+        const result = ((zeroBasedLetter + numShift) % 26) + 65;
+        shiftedChar = String.fromCharCode(result);
+      }
+
+      result += shiftedChar;
+    } else {
+      // otherwise.... do nothing
+      result += str[i];
+    }
+  }
+
+  // letters
+  // a - 97, z - 122
+  // A - 65, Z - 90
+
+  // letter a by shift 3
+  // (0 + 3) mod 26 = 3 -- d
+  // (97 + 3) mode 26 =
+  // 0 로 만들면 되지않냐? -97
+
+  // A - 0, B - 1, ...., Z - 25
+  // letter x by shift n
+  // (x + n)  mod 26
+
+  // letter z by shift 1
+  // (25 + 1) mod 26 = 0 -- a
+
+  // letter z by shift 27
+  // (25 + 27) mod 26 = 0 -- a
+
+  // Something that is ciphered
+  return result;
+}
+
+function shiftChar(char) {}
+
+function isLowerCase(char) {
+  if (char.charCodeAt() >= 97 && char.charCodeAt() <= 122) {
+    return true;
+  }
+
+  return false;
+}
+
+function isUpperCase(char) {
+  if (char.charCodeAt() >= 65 && char.charCodeAt() <= 90) {
+    return true;
+  }
+
+  return false;
+}
+
+function isCharacterALetter(char) {
+  return /[a-zA-Z]/.test(char);
+}
+
+export { caesar };
---
+++ caesar.test.js
@@ -0,0 +1,18 @@
+import { caesar } from "./caesar";
+
+// Don’t forget to test wrapping from z to a.
+// Don’t forget to test keeping the same case.
+// Don’t forget to test punctuation!
+
+test("caesar cipher", () => {
+  expect(caesar("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG", 23)).toBe(
+    "QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD"
+  );
+  expect(caesar("z", 1)).toBe("a");
+  expect(caesar("aBc", 1)).toBe("bCd");
+  expect(caesar("abc!", 1)).toBe("bcd!");
+
+  // I need to pass this test case
+  expect(caesar("a", -1)).toBe("z");
+  expect(caesar("a!", -1)).toBe("z!");
+});
